b91f27c09371535e1c0208e053dedf3e9057f46f
Hask <hask@586989-oiptwvds.tmweb.ru>
2021-09-04 19:15:42 +0300

    I added some types

--- src/Type/Response.hs
+++ src/Type/Response.hs
@@ -7,12 +7,20 @@ type ListUsers = Map.Map Int Int
 
 type UpdateId = Int
 type JustId = Int
+type Message = T.Text
+type NumRepeat = T.Text
+type FileId = T.Text
 
-data RespText = RespTex UpdateId JustId T.Text deriving (Show, Eq)
-data RespButton = RespButton UpdateId JustId T.Text deriving (Show, Eq)
-data RespStiker = RespStiker UpdateId JustId T.Text deriving (Show, Eq)
+data RespText = RespText UpdateId JustId Message deriving (Show, Eq)
+data RespButton = RespButton UpdateId JustId NumRepeat deriving (Show, Eq)
+data RespStiker = RespStiker UpdateId JustId FileId deriving (Show, Eq)
 data RespEmpty = RespEmpty () deriving (Show, Eq)
 
 class Response a where
   getUpdateId :: a -> UpdateId
   getJustId :: a -> JustId
+
+instance Response RespText where
+  getUpdateId (RespText upId _ _) = upId 
+  getJustId (RespText _ jId _) = jId
+
