607eef34a8adee843ba07c5431fa3c16ab7a0140
Renat Ishmur <jeingo20@gmail.com>
2021-09-07 22:03:21 +0300

    I have added handling of Video , Voice , Document , Photo.

--- src/App/Request.hs
+++ src/App/Request.hs
@@ -33,10 +33,20 @@ parseResponseToMyType respTmp
   | respT /= Nothing = RT (fromJust respT) 
   | respB /= Nothing = RB (fromJust respB) 
   | respS /= Nothing = RS (fromJust respS)
+  | respVd /= Nothing = RVd (fromJust respVd)
+  | respVo /= Nothing = RVo (fromJust respVo)
+  | respD /= Nothing = RD (fromJust respD)
+  | respP /= Nothing = RP (fromJust respP)
+  | respA /= Nothing = RA (fromJust respA)
   where respT = decodeStrict respTmp :: Maybe RespText
         respB = decodeStrict respTmp :: Maybe RespButton
         respS = decodeStrict respTmp :: Maybe RespStiker
+        respVd = decodeStrict respTmp :: Maybe RespVideo
+        respVo = decodeStrict respTmp :: Maybe RespVoice
+        respD = decodeStrict respTmp :: Maybe RespDocument
+        respP = decodeStrict respTmp :: Maybe RespPhoto
         respE = decodeStrict respTmp :: Maybe RespEmpty
+        respA = decodeStrict respTmp :: Maybe RespAnyForOffset
 
 -- send Request
 
@@ -45,6 +55,8 @@ sendEcho :: ResponseAll -> ConfData -> ListUsers -> Token -> IO ()
 sendEcho (RE resp) conf listUsers token = return ()
 
 sendEcho (RB resp) conf listUsers token = return ()
+
+sendEcho (RA resp) conf listUsers token = return ()
 
 sendEcho (RT resp) conf listUsers token = do
   let numOfRepeat = fromJust $ Map.lookup (getJustId resp) listUsers 
@@ -59,12 +71,37 @@ sendEcho (RS resp) conf listUsers token = do
   sendStikers resp token numOfRepeat
   return () 
 
+sendEcho (RVd resp) conf listUsers token = do
+  let numOfRepeat = fromJust $ Map.lookup (getJustId resp) listUsers 
+  sendVideo resp token numOfRepeat
+  return () 
+
+sendEcho (RVo resp) conf listUsers token = do
+  let numOfRepeat = fromJust $ Map.lookup (getJustId resp) listUsers 
+  sendVoice resp token numOfRepeat
+  return () 
+
+sendEcho (RD resp) conf listUsers token = do
+  let numOfRepeat = fromJust $ Map.lookup (getJustId resp) listUsers 
+  sendDocument resp token numOfRepeat
+  return () 
+
+sendEcho (RP resp) conf listUsers token = do
+  let numOfRepeat = fromJust $ Map.lookup (getJustId resp) listUsers 
+  sendPhoto resp token numOfRepeat
+  return () 
+
 -- get offset and next step
 
 getOffset :: ResponseAll -> Offset 
 getOffset (RT a) = show $ ( getUpdateId a ) + 1 
 getOffset (RB a) = show $ ( getUpdateId a ) + 1 
 getOffset (RS a) = show $ ( getUpdateId a ) + 1 
+getOffset (RA a) = show $ ( getUpdateId a ) + 1
+getOffset (RVd a) = show $ ( getUpdateId a ) + 1
+getOffset (RVo a) = show $ ( getUpdateId a ) + 1
+getOffset (RD a) = show $ ( getUpdateId a ) + 1
+getOffset (RP a) = show $ ( getUpdateId a ) + 1
 getOffset (RE a) = " "
 
 nextStepRequest :: Offset -> Token -> IO ()
@@ -105,10 +142,43 @@ sendMessages resp (Token token) numOfRepeat = do
 sendStikers :: RespStiker -> Token -> Int -> IO ()
 sendStikers resp (Token token) numOfRepeat = do
   let chId = show $ getJustId resp
-  let stikerId = T.unpack $ getStikerId resp
-  let req = urlApiTelegram ++ token ++ "/sendAnimation" ++ "?chat_id=" ++ chId ++ "&animation=" ++  stikerId 
+  let fileId = T.unpack $ getFileId resp
+  let req = urlApiTelegram ++ token ++ "/sendAnimation" ++ "?chat_id=" ++ chId ++ "&animation=" ++  fileId 
   replicateM_ numOfRepeat $ N.httpNoBody $ N.parseRequest_ req
   return ()
+
+sendVideo :: RespVideo -> Token -> Int -> IO ()
+sendVideo resp (Token token) numOfRepeat = do
+  let chId = show $ getJustId resp
+  let fileId = T.unpack $ getFileId resp
+  let req = urlApiTelegram ++ token ++ "/sendVideo" ++ "?chat_id=" ++ chId ++ "&video=" ++  fileId 
+  replicateM_ numOfRepeat $ N.httpNoBody $ N.parseRequest_ req
+  return ()
+  
+sendVoice :: RespVoice -> Token -> Int -> IO ()
+sendVoice resp (Token token) numOfRepeat = do
+  let chId = show $ getJustId resp
+  let fileId = T.unpack $ getFileId resp
+  let req = urlApiTelegram ++ token ++ "/sendVoice" ++ "?chat_id=" ++ chId ++ "&voice=" ++  fileId 
+  replicateM_ numOfRepeat $ N.httpNoBody $ N.parseRequest_ req
+  return ()
+
+sendDocument :: RespDocument -> Token -> Int -> IO ()
+sendDocument resp (Token token) numOfRepeat = do
+  let chId = show $ getJustId resp
+  let fileId = T.unpack $ getFileId resp
+  let req = urlApiTelegram ++ token ++ "/sendDocument" ++ "?chat_id=" ++ chId ++ "&document=" ++  fileId 
+  replicateM_ numOfRepeat $ N.httpNoBody $ N.parseRequest_ req
+  return ()
+
+sendPhoto :: RespPhoto -> Token -> Int -> IO ()
+sendPhoto resp (Token token) numOfRepeat = do
+  let chId = show $ getJustId resp
+  let fileId = T.unpack $ getFileId resp
+  let req = urlApiTelegram ++ token ++ "/sendPhoto" ++ "?chat_id=" ++ chId ++ "&photo=" ++  fileId 
+  replicateM_ numOfRepeat $ N.httpNoBody $ N.parseRequest_ req
+  return ()
+
 
 -- add new user
 
@@ -121,10 +191,28 @@ addNewUser (RS resp) listUsers numOfRepeat = if Map.member (getJustId resp) list
                                         then listUsers
                                         else Map.insert (getJustId resp) numOfRepeat listUsers
 
+addNewUser (RVd resp) listUsers numOfRepeat = if Map.member (getJustId resp) listUsers
+                                        then listUsers
+                                        else Map.insert (getJustId resp) numOfRepeat listUsers
+
+addNewUser (RVo resp) listUsers numOfRepeat = if Map.member (getJustId resp) listUsers
+                                        then listUsers
+                                        else Map.insert (getJustId resp) numOfRepeat listUsers
+
+addNewUser (RD resp) listUsers numOfRepeat = if Map.member (getJustId resp) listUsers
+                                        then listUsers
+                                        else Map.insert (getJustId resp) numOfRepeat listUsers
+
+addNewUser (RP resp) listUsers numOfRepeat = if Map.member (getJustId resp) listUsers
+                                        then listUsers
+                                        else Map.insert (getJustId resp) numOfRepeat listUsers
+
 addNewUser (RB resp) listUsers numOfRepeat = Map.insert (getJustId resp) num listUsers 
   where num = read $ T.unpack (getNumRepeat resp) :: Int 
 
 addNewUser (RE resp) listUsers numOfRepeat = listUsers 
+
+addNewUser (RA resp) listUsers numOfRepeat = listUsers
 
 
 
--- src/Type/Response.hs
+++ src/Type/Response.hs
@@ -18,42 +18,73 @@ type JustId = Int
 type Offset = String 
 type Message = T.Text
 type NumRepeat = T.Text
-type StikerId = T.Text
+type FileId = T.Text
 type HelpText = String
 type RepeatText = String
 type KeyBoardJson = String
 
 
-data ResponseAll = RT RespText | RB RespButton | RS RespStiker | RE RespEmpty deriving (Show, Eq)
+data ResponseAll = RT RespText | RB RespButton | RS RespStiker | RE RespEmpty | RA RespAnyForOffset | RVd RespVideo | RVo RespVoice | RD RespDocument | RP RespPhoto deriving (Show, Eq)
 data RespText = RespText UpdateId JustId Message deriving (Show, Eq)
 data RespButton = RespButton UpdateId JustId NumRepeat deriving (Show, Eq)
-data RespStiker = RespStiker UpdateId JustId StikerId deriving (Show, Eq)
+data RespStiker = RespStiker UpdateId JustId FileId deriving (Show, Eq)
+data RespVideo = RespVideo UpdateId JustId FileId deriving (Show, Eq)
+data RespVoice = RespVoice UpdateId JustId FileId deriving (Show, Eq)
+data RespDocument = RespDocument UpdateId JustId FileId deriving (Show, Eq)
+data RespPhoto = RespPhoto UpdateId JustId FileId deriving (Show, Eq)
 data RespEmpty = RespEmpty () deriving (Show, Eq)
+data RespAnyForOffset = RespAny UpdateId deriving (Show, Eq)
 
 class Response a where
   getUpdateId :: a -> UpdateId
   getJustId :: a -> JustId
+  getFileId :: a -> FileId
     
 instance Response RespText where
   getUpdateId (RespText upId _ _) = upId 
   getJustId (RespText _ jId _) = jId
+  getFileId a = " "
  
 instance Response RespButton where
   getUpdateId (RespButton upId _ _) = upId 
   getJustId (RespButton _ jId _) = jId
+  getFileId a = " "
 
 instance Response RespStiker where
   getUpdateId (RespStiker upId _ _) = upId 
   getJustId (RespStiker _ jId _) = jId
+  getFileId (RespStiker _ _ stikId) = stikId
+
+instance Response RespAnyForOffset where
+  getUpdateId (RespAny upId) = upId 
+  getJustId (RespAny _) = 0
+  getFileId a = " "
+
+instance Response RespVideo where
+  getUpdateId (RespVideo upId _ _) = upId 
+  getJustId (RespVideo _ jId _) = jId
+  getFileId (RespVideo _ _ vidId) = vidId
+
+instance Response RespVoice where
+  getUpdateId (RespVoice upId _ _) = upId 
+  getJustId (RespVoice _ jId _) = jId
+  getFileId (RespVoice _ _ voId) = voId
+    
+instance Response RespDocument where
+  getUpdateId (RespDocument upId _ _) = upId 
+  getJustId (RespDocument _ jId _) = jId
+  getFileId (RespDocument _ _ docId) = docId
+  
+instance Response RespPhoto where
+  getUpdateId (RespPhoto upId _ _) = upId 
+  getJustId (RespPhoto _ jId _) = jId
+  getFileId (RespPhoto _ _ phId) = phId
 
 getMessage :: RespText -> Message
 getMessage (RespText _ _ mess) = mess
 
 getNumRepeat :: RespButton -> NumRepeat
 getNumRepeat (RespButton _ _ numR) = numR
-
-getStikerId :: RespStiker -> StikerId
-getStikerId (RespStiker _ _ stkId) = stkId
 
 --Json
 
@@ -105,5 +136,68 @@ instance FromJSON RespEmpty where
 
   parseJSON _ = mzero
 
+instance FromJSON RespAnyForOffset where
+  parseJSON (Object req) = do
+    result <- req .: "result"
+    let arr = V.head result
+    upId <- arr .: "update_id"
+    return $ RespAny upId
 
+  parseJSON _ = mzero
 
+instance FromJSON RespVideo where
+  parseJSON (Object req) = do 
+    result <- req .: "result"
+    let arr = V.head result
+    upId <- arr .: "update_id"
+    mes <- arr .: "message"
+    video <- mes .: "video" 
+    from <- mes .: "from"
+    jId <- from .: "id"
+    fId <- video .: "file_id"
+    return $ RespVideo upId jId fId 
+
+  parseJSON _ = mzero
+
+instance FromJSON RespVoice where
+  parseJSON (Object req) = do 
+    result <- req .: "result"
+    let arr = V.head result
+    upId <- arr .: "update_id"
+    mes <- arr .: "message"
+    voice <- mes .: "voice" 
+    from <- mes .: "from"
+    jId <- from .: "id"
+    fId <- voice .: "file_id"
+    return $ RespVoice upId jId fId 
+
+  parseJSON _ = mzero
+
+instance FromJSON RespDocument where
+  parseJSON (Object req) = do 
+    result <- req .: "result"
+    let arr = V.head result
+    upId <- arr .: "update_id"
+    mes <- arr .: "message"
+    doc <- mes .: "document" 
+    from <- mes .: "from"
+    jId <- from .: "id"
+    fId <- doc .: "file_id"
+    return $ RespDocument upId jId fId 
+
+  parseJSON _ = mzero
+
+instance FromJSON RespPhoto where
+  parseJSON (Object req) = do 
+    result <- req .: "result"
+    let arr = V.head result
+    upId <- arr .: "update_id"
+    mes <- arr .: "message"
+    photo <- mes .: "photo" 
+    from <- mes .: "from"
+    jId <- from .: "id"
+    let fstPh = V.head photo 
+    fId <- fstPh .: "file_id"
+    return $ RespPhoto upId jId fId 
+
+  parseJSON _ = mzero
