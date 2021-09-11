f47ba021ade61120e0b84812c24445b0e8b0749f
Renat Ishmur <jeingo20@gmail.com>
2021-09-11 14:15:41 +0300

    I added 2 tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,6 +5,7 @@ import Test.Hspec
 import qualified Data.Map as Map
 
 import App.Config
+import App.Request
 import Type.Response
 
 main :: IO ()
@@ -19,9 +20,43 @@ token = getToken conf
 conf :: ConfData
 conf = ConfData "helpText" "repeatText" 3 "buttonJson" (Token "12345token")
 
+responseText :: ResponseAll
+responseText = RT $ RespText  123 1234 "message"
+
+responseDocument :: ResponseAll
+responseDocument = RD $ RespDocument 234 2345 "fileId234"
+
+responseButton :: ResponseAll
+responseButton = RB $ RespButton 345 3456 "1"
+
+responseEmpty :: ResponseAll
+responseEmpty = RE $ RespEmpty ()
+
+responsePhoto :: ResponseAll
+responsePhoto = RP $ RespPhoto 456 4567 "fileId456"
+
 test :: Spec
 test = do
-  describe "Tests : " $ do
-    it "test 1 :" $ do
-           let result = token
-           result `shouldBe` token 
+  describe "Test function addNewUser : " $ do
+    it "test - add new user (Text)" $ do
+      let result = addNewUser responseText listUsers (startRepeat conf) 
+      result `shouldBe` Map.singleton 1234 3 
+    it "test - add new user (Document)" $ do
+      let result = addNewUser responseDocument listUsers (startRepeat conf) 
+      result `shouldBe` Map.singleton 2345 3 
+    it "test - add new user (Button)" $ do
+      let result = addNewUser responseButton listUsers (startRepeat conf) 
+      result `shouldBe` Map.singleton 3456 1
+    it "test - add new user (Empty)" $ do
+      let result = addNewUser responseEmpty listUsers (startRepeat conf) 
+      result `shouldBe` Map.empty 
+  describe "Test function getOffset : " $ do
+    it "test - get offset (Text)" $ do
+      let result = getOffset responseText
+      result `shouldBe` "124"
+    it "test - get offset (Photo)" $ do
+      let result = getOffset responsePhoto
+      result `shouldBe` "457"
+    it "test - get offset (Empty)" $ do
+      let result = getOffset responseEmpty
+      result `shouldBe` " "
